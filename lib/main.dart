9787fb13312a5d488805fc27f774a41b1cd849ca
RobertBenyamin <robertbenyamino@gmail.com>
2024-02-24 00:22:42 +0700

    chore: add Google Fonts dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/ui/home.dart';
 
 void main() {
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       home: const HomePage(),
     );
