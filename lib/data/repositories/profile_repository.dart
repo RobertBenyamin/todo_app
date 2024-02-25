53386a3188fee50289086e4b3da040c004ae5b4e
RobertBenyamin <robertbenyamino@gmail.com>
2024-02-25 23:23:32 +0700

    feat: create profile repository

---
+++ lib/data/repositories/profile_repository.dart
@@ -0,0 +1,41 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfileRepository {
+  static late SharedPreferences sharedPreferences;
+
+  static Future<void> init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future<String> getName() async {
+    return sharedPreferences.getString('name') ?? "";
+  }
+
+  Future<void> setName(String name) async {
+    await sharedPreferences.setString('name', name);
+  }
+
+  Future<String> getMajor() async {
+    return sharedPreferences.getString('major') ?? "";
+  }
+
+  Future<void> setMajor(String major) async {
+    await sharedPreferences.setString('major', major);
+  }
+
+  Future<String> getDateOfBirth() async {
+    return sharedPreferences.getString('dateOfBirth') ?? "";
+  }
+
+  Future<void> setDateOfBirth(String dateOfBirth) async {
+    await sharedPreferences.setString('dateOfBirth', dateOfBirth);
+  }
+
+  Future<String> getEmail() async {
+    return sharedPreferences.getString('email') ?? "";
+  }
+
+  Future<void> setEmail(String email) async {
+    await sharedPreferences.setString('email', email);
+  }
+}
